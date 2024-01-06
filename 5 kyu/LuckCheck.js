303041c360d7e6faa5a663739326c053f05594a8
JonSEK <engkien@ymail.com>
2024-01-06 17:14:42 +0800

    Return true if sum of digits on the left half of its number was equal to the sum of digits on the right half

---
+++ 5 kyu/LuckCheck.js
@@ -0,0 +1,30 @@
+/* 
+In some countries of former Soviet Union there was a belief about lucky tickets. 
+A transport ticket of any sort was believed to posess luck if sum of digits on the left half of its number was equal to the sum of digits on the right half. 
+
+Here are examples of such numbers:
+003111    #             3 = 1 + 1 + 1
+813372    #     8 + 1 + 3 = 3 + 7 + 2
+17935     #         1 + 7 = 3 + 5  // if the length is odd, you should ignore the middle number when adding the halves.
+56328116  # 5 + 6 + 3 + 2 = 8 + 1 + 1 + 6
+
+Such tickets were either eaten after being used or collected for bragging rights.
+Your task is to write a funtion luck_check(str), which returns true/True if argument is string decimal representation of a lucky ticket number, or false/False for all other numbers. 
+It should throw errors for empty strings or strings which don't represent a decimal number. 
+*/
+
+function luckCheck(ticket) {
+  if (ticket.length === 0) throw new Error("Empty string");
+  if (ticket.match(/\D/)) throw new Error("Not a decimal number");
+  let sum = 0;
+  let half = Math.floor(ticket.length / 2);
+  for (let i = 0; i < half; i++) {
+    sum += parseInt(ticket[i]);
+    sum -= parseInt(ticket[ticket.length - i - 1]);
+  }
+  return sum === 0;
+}
+
+console.log(luckCheck("003111"));
+console.log(luckCheck("813372"));
+console.log(luckCheck("17935"));
