235f1c589268a5b23bf793d239cce262fb3de533
JonSEK <engkien@ymail.com>
2024-01-27 22:00:00 +0800

    Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed

---
+++ 6 kyu/StopGninnipsMySdrow.js
@@ -0,0 +1,23 @@
+/* 
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+*/
+
+function spinWords(string) {
+  let words = string.split(" ");
+  let newWords = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      let newWord = words[i].split("").reverse().join("");
+      newWords.push(newWord);
+    } else {
+      newWords.push(words[i]);
+    }
+  }
+  return newWords.join(" ");
+}
