284cc4ed29572a743c41dc6249ba950fd930d9ac
JonSEK <engkien@ymail.com>
2024-02-11 22:00:00 +0800

    Given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.

---
+++ 6 kyu/ArrayCombinations.js
@@ -0,0 +1,10 @@
+/* 
+Given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+See test cases for more examples.
+*/
+
+function solve(arr) {
+  return arr.map((x) => [...new Set(x)].length).reduce((a, b) => a * b, 1);
+}
