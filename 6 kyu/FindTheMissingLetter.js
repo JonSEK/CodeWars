fa7dfb0c649c19c4a7f7ba5b105c3101348c94c2
JonSEK <engkien@ymail.com>
2024-02-18 22:00:00 +0800

    Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.

---
+++ 6 kyu/FindTheMissingLetter.js
@@ -0,0 +1,22 @@
+/* 
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P' 
+*/
+
+function findMissingLetter(array) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let start = alphabet.indexOf(array[0].toLowerCase());
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].toLowerCase() !== alphabet[start + i]) {
+      return array[i].toLowerCase() === array[i]
+        ? alphabet[start + i]
+        : alphabet[start + i].toUpperCase();
+    }
+  }
+  return "";
+}
