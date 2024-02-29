c8c7c2c999b178691a6b8163a94cbf712e7fd2f3
JonSEK <engkien@ymail.com>
2024-02-29 08:25:16 +0800

    Given Two integers a, b, find The sum of them, BUT You are not allowed to use the operators + and -

---
+++ 6 kyu/SumOfTwoIntegers.js
@@ -0,0 +1,22 @@
+/* 
+Given Two integers a, b, find The sum of them, BUT You are not allowed to use the operators + and -
+
+Notes
+The numbers (a,b) may be positive, negative values or zeros.
+Returning value will be an integer.
+Javascript: the Array reduce methods are disabled, along with eval, require, and module.
+
+Examples
+1- Add (5,19) ==> return (24) 
+2- Add (-27,18) ==> return (-9)
+3- Add (-14,-16) ==> return (-30) 
+*/
+
+function add(x, y) {
+  while (y) {
+    let carry = x & y;
+    x = x ^ y;
+    y = carry << 1;
+  }
+  return x;
+}
